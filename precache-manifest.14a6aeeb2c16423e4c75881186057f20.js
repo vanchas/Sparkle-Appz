d30f22a569ff350eef5ce0be95c3e11f02b687a1
Vancha <46772645+vanchas@users.noreply.github.com>
2020-02-13 20:17:55 +0200

    Updates

---
+++ precache-manifest.14a6aeeb2c16423e4c75881186057f20.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c081f79070c4f39fd915e0fe54516f03",
+    "url": "/Sparkle-Appz/index.html"
+  },
+  {
+    "revision": "d43d610e34bd3926a8f7",
+    "url": "/Sparkle-Appz/static/css/2.83870dc8.chunk.css"
+  },
+  {
+    "revision": "54ca0026734888b8bd50",
+    "url": "/Sparkle-Appz/static/css/main.a59de8ff.chunk.css"
+  },
+  {
+    "revision": "d43d610e34bd3926a8f7",
+    "url": "/Sparkle-Appz/static/js/2.958d28df.chunk.js"
+  },
+  {
+    "revision": "e933f8dbfdf496cb72e495663a499a00",
+    "url": "/Sparkle-Appz/static/js/2.958d28df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54ca0026734888b8bd50",
+    "url": "/Sparkle-Appz/static/js/main.8b2f6289.chunk.js"
+  },
+  {
+    "revision": "e3671ef512a4e816c678",
+    "url": "/Sparkle-Appz/static/js/runtime-main.70810c29.js"
+  },
+  {
+    "revision": "2182330384ebbad826821fdf8e84efe5",
+    "url": "/Sparkle-Appz/static/media/react-bootstrap-carousel.21823303.eot"
+  },
+  {
+    "revision": "c746c4882df00af3b0a7a3b1616bf295",
+    "url": "/Sparkle-Appz/static/media/react-bootstrap-carousel.c746c488.woff"
+  },
+  {
+    "revision": "f7448c0408e7a78012a82def0fb9e2fe",
+    "url": "/Sparkle-Appz/static/media/react-bootstrap-carousel.f7448c04.ttf"
+  },
+  {
+    "revision": "f86d9bf34b5da8610c6a218e96667b68",
+    "url": "/Sparkle-Appz/static/media/react-bootstrap-carousel.f86d9bf3.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Sparkle-Appz/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Sparkle-Appz/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Sparkle-Appz/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Sparkle-Appz/static/media/slick.f97e3bbf.svg"
+  }
+]);
