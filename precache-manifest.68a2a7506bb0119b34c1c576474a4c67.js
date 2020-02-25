ad5065c01133e4fdfdc4f356f4f4071686aa3883
Ivan <vanchas@protonmail.com>
2020-02-25 10:27:45 +0200

    Updates

--- precache-manifest.68a2a7506bb0119b34c1c576474a4c67.js
+++ precache-manifest.68a2a7506bb0119b34c1c576474a4c67.js
@@ -1,35 +1,39 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "60729a06d3dbe8a2760e19b5d4af758b",
+    "revision": "1a8ff8744d601433adb5b4a2a400483b",
     "url": "/Sparkle-Appz/index.html"
   },
   {
-    "revision": "35cc4100d2dbeb7b8efa",
-    "url": "/Sparkle-Appz/static/css/2.83870dc8.chunk.css"
+    "revision": "859c28a5de4bca64da6e",
+    "url": "/Sparkle-Appz/static/css/2.6e2fb314.chunk.css"
   },
   {
-    "revision": "8809710f7c36a3be8262",
-    "url": "/Sparkle-Appz/static/css/main.d9e478b8.chunk.css"
+    "revision": "70bd30f9c933dd1335c1",
+    "url": "/Sparkle-Appz/static/css/main.b3a23085.chunk.css"
   },
   {
-    "revision": "35cc4100d2dbeb7b8efa",
-    "url": "/Sparkle-Appz/static/js/2.737f2f41.chunk.js"
+    "revision": "859c28a5de4bca64da6e",
+    "url": "/Sparkle-Appz/static/js/2.2520cf28.chunk.js"
   },
   {
     "revision": "fa942519a7a9feed579ab048ae65ea42",
-    "url": "/Sparkle-Appz/static/js/2.737f2f41.chunk.js.LICENSE.txt"
+    "url": "/Sparkle-Appz/static/js/2.2520cf28.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8809710f7c36a3be8262",
-    "url": "/Sparkle-Appz/static/js/main.1ffb14ee.chunk.js"
+    "revision": "70bd30f9c933dd1335c1",
+    "url": "/Sparkle-Appz/static/js/main.c4105a86.chunk.js"
   },
   {
-    "revision": "e3671ef512a4e816c678",
-    "url": "/Sparkle-Appz/static/js/runtime-main.70810c29.js"
+    "revision": "3767a50e0da0e5e6f7a2",
+    "url": "/Sparkle-Appz/static/js/runtime-main.bd37ffba.js"
   },
   {
     "revision": "2182330384ebbad826821fdf8e84efe5",
     "url": "/Sparkle-Appz/static/media/react-bootstrap-carousel.21823303.eot"
+  },
+  {
+    "revision": "2fe6d179ea5aa1ceab26113e77e13da4",
+    "url": "/Sparkle-Appz/static/media/react-bootstrap-carousel.2fe6d179.svg"
   },
   {
     "revision": "c746c4882df00af3b0a7a3b1616bf295",
@@ -38,10 +42,6 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "f7448c0408e7a78012a82def0fb9e2fe",
     "url": "/Sparkle-Appz/static/media/react-bootstrap-carousel.f7448c04.ttf"
-  },
-  {
-    "revision": "f86d9bf34b5da8610c6a218e96667b68",
-    "url": "/Sparkle-Appz/static/media/react-bootstrap-carousel.f86d9bf3.svg"
   },
   {
     "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
